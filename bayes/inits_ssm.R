b96480733e9b6d6254db0e3a36ab3d3d054d4a60
AkiraTerui <hanabi0111@gmail.com>
2021-03-22 13:35:49 -0400

    edits: inits_ssm, formatting scripts

--- bayes/inits_ssm.R
+++ bayes/inits_ssm.R
@@ -1,90 +1,127 @@
 
 # setup -------------------------------------------------------------------
 
-  pacman::p_load(tidyverse, runjags)
+  pacman::p_load(tidyverse, runjags, foreach)
   source(here::here("data_fmt_watershed.R"))
   
   n_ad <- 100
-  n_iter <- 1E+4
+  n_iter <- 500
   n_thin <- max(3, ceiling(n_iter/500))
   n_burn <- ceiling(max(10, n_iter/2))
   n_sample <- ceiling(n_iter/n_thin)
 
+# define function ---------------------------------------------------------
 
-# prepare data ------------------------------------------------------------
-
-  dat <- dat_list[[2]]
-  
-  dat <- dat %>% 
-    mutate(river_id = as.numeric(as.factor(river)))
-  
-  # data
-  ## data
-  Y <- dat$sample1 + dat$sample2
-  log_Area <- log(dat$area)
-  
-  ## year, river, site indicators
-  Year <- dat$year + 1 - min(dat$year, na.rm = T)
-  River <- dat$river_id
-  Site <- dat$site
-  
-  ## n iterators
-  N_sample <- nrow(dat)
-  N_year <- n_distinct(dat$year)
-  N_river <- n_distinct(dat$river)
-  N_site <- dat %>% 
-    group_by(river_id) %>% 
-    arrange(river_id) %>% 
-    summarize(n_site = n_distinct(site)) %>% 
-    pull(n_site)
+  rm_bracket <- function(string) {
+    x <- str_extract(string, "\\[.{1,}\\]")
+    y <- str_remove_all(x, "\\[|\\]")
+    return(y)
+  }
   
 
-# jags setup --------------------------------------------------------------
+# run analysis ------------------------------------------------------------
 
-  inits <- replicate(3,
-                     list(
-                       mean_log_r = rep(0, N_river),
-                       tau_eps_site = 1,
-                       .RNG.name = "base::Mersenne-Twister",
-                       .RNG.seed = NA
-                       ),
-                     simplify = FALSE)
+  joint_summary <- foreach(i = 1:length(dat_list), .combine = bind_rows) %do% {
+      
+    # prepare data ------------------------------------------------------------
+    
+    dat <- dat_list[[i]]
+      
+    dat <- dat %>% 
+      mutate(river_id = as.numeric(as.factor(river)))
+    
+    # data
+    ## data
+    Y <- dat$sample1 + dat$sample2
+    log_Area <- log(dat$area)
+    
+    ## year, river, site indicators
+    Year <- dat$year + 1 - min(dat$year, na.rm = T)
+    River <- dat$river_id
+    Site <- dat$site
+    
+    ## n iterators
+    N_sample <- nrow(dat)
+    N_year <- n_distinct(dat$year)
+    N_river <- n_distinct(dat$river)
+    N_site <- dat %>% 
+      group_by(river_id) %>% 
+      arrange(river_id) %>% 
+      summarize(n_site = n_distinct(site)) %>% 
+      pull(n_site)
+    
   
-  for (i in 1:3) inits[[i]]$.RNG.seed <- i + 100
+    # jags setup --------------------------------------------------------------
   
-  d_jags <- list(Y = Y,
-                 log_Area = log_Area,
-                 Year = Year,
-                 River = River,
-                 Site = Site,
-                 N_sample = N_sample,
-                 N_year = N_year,
-                 N_river = N_river,
-                 N_site = N_site)
+    inits <- replicate(3,
+                       list(
+                         log_r_mean = rep(0, N_river),
+                         tau_eps_site = 1,
+                         .RNG.name = "base::Mersenne-Twister",
+                         .RNG.seed = NA
+                         ),
+                       simplify = FALSE)
+    
+    for (i in 1:3) inits[[i]]$.RNG.seed <- i + 100
+    
+    d_jags <- list(Y = Y,
+                   log_Area = log_Area,
+                   Year = Year,
+                   River = River,
+                   Site = Site,
+                   N_sample = N_sample,
+                   N_year = N_year,
+                   N_river = N_river,
+                   N_site = N_site)
+    
+    para <- c("log_r_global",
+              "log_r_mean",
+              "sigma_eps_r",
+              "sigma_eps_site",
+              "log_d_mean")
+    
+    m <- read.jagsfile("bayes/model_ssm.R")
   
-  para <- c("mu_log_area",
-            "sigma_log_area",
-            "mean_log_r",
-            "sigma_eps_r",
-            "sigma_eps_site")
-  
-  m <- read.jagsfile("bayes/model_ssm.R")
-
-  
-# run jags ----------------------------------------------------------------
-  
-  post <- run.jags(m$model,
-                   monitor = para,
-                   data = d_jags,
-                   n.chains = 3,
-                   inits = inits,
-                   method = "parallel",
-                   burnin = n_burn,
-                   sample = n_sample,
-                   adapt = n_ad,
-                   thin = n_thin,
-                   n.sims = 3,
-                   module = "glm")
-  
-  MCMCvis::MCMCsummary(post$mcmc)
+    
+    # run jags ----------------------------------------------------------------
+    
+    post <- run.jags(m$model,
+                     monitor = para,
+                     data = d_jags,
+                     n.chains = 3,
+                     inits = inits,
+                     method = "parallel",
+                     burnin = n_burn,
+                     sample = n_sample,
+                     adapt = n_ad,
+                     thin = n_thin,
+                     n.sims = 3,
+                     module = "glm")
+    
+    mcmc_summary <- MCMCvis::MCMCsummary(post$mcmc)
+    
+    # format results
+    mcmc_tibble <- mcmc_summary %>% 
+      as_tibble() %>% 
+      mutate(species = unique(dat$LatinName),
+             param = rownames(mcmc_summary)) %>% 
+      mutate(param_id = str_remove_all(param, "\\[.{1,}\\]")) %>% 
+      mutate(river_id = case_when(param_id != "log_d_mean" ~ rm_bracket(param),
+                                  param_id == "log_d_mean" ~ str_remove(rm_bracket(param),
+                                                                        "(.{1,},)"))
+             ) %>% 
+      mutate(year_id = case_when(param_id == "log_d_mean" ~ str_remove(rm_bracket(param),
+                                                                       "(,.{1,})"),
+                                 TRUE ~ as.character(NA))
+             ) %>% 
+      mutate(river_id = as.numeric(river_id),
+             year_id = as.numeric(year_id))
+    
+    mcmc_tibble <- dat %>% 
+      group_by(river) %>% 
+      summarize(river_id = unique(river_id)) %>% 
+      right_join(mcmc_tibble, by = "river_id")
+    
+    return(mcmc_tibble)
+  }
   
